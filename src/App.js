96b52b8a51652c567ef476a417ff44157e6266b2
jonathan <jonathankendrick697@gmail.com>
2022-11-01 04:13:54 -0700

    deploy app

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           <a href='https://twitter.com/jonatha62141643?t=x5Xm8d_OUq0obCaBHgUNfQ&s=08' className='link'>Twitter Link</a>
           <a href='https://training.zuri.team/' className='link' id='btn__zuri'>Zuri Team</a>
           <a href='http://books.zuri.team' className='link' id='books'>Zuri Books</a>
-          <a href='https://books.zuri.team/python-for-beginners?ref_id=okeibunoremma' className='link' id='book__python'>Python Books</a>
+          <a href='https://books.zuri.team/python-for-beginners' className='link' id='book__python'>Python Books</a>
           <a href='https://background.zuri.team' className='link' id='pitch'>Background Check for Coders</a>
           <a href='https://books.zuri.team/design-rules' className='link' id='book__design'>Design Books</a>
         </div>
